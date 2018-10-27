81b4d065e8f6632170344740db9ab3effc1f00d0
tanks <tanks@DESKTOP-OJ076EI>
2018-10-27 21:50:57 +0200

    fixed send mail when checkout

--- src/java/controller/CheckoutController.java
+++ src/java/controller/CheckoutController.java
@@ -15,11 +15,15 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import model.NavigationBar;
 import model.OrderDetail;
 import model.Orders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +36,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/checkout/")
 public class CheckoutController {
+
+    @Autowired
+    JavaMailSender mailer1;
 
     @RequestMapping(value = "checkout")
     public String view(ModelMap model, HttpServletRequest request) {
@@ -70,13 +77,33 @@ public class CheckoutController {
         Orders orders = new Orders(total, now.toString(), name, phone, address, note, userid, 1, 1);
         CheckoutDAO list = new CheckoutDAO();
         list.add_order(orders);
-        
+
         CartBean cartBean = (CartBean) session.getAttribute("SHOP");
-        for (ProductDTO productDTO : cartBean.values()){
+        for (ProductDTO productDTO : cartBean.values()) {
             OrderDetail orderdetail = new OrderDetail(productDTO.getQuantity(), list.select_id_just_added_to_order(), productDTO.getSanpham().getId());
             list.add_orderdetail(orderdetail);
         }
-        
+
+        try {
+            // Tạo mail
+            MimeMessage mail = mailer1.createMimeMessage();
+            // Sử dụng lớp trợ giúp
+            MimeMessageHelper helper = new MimeMessageHelper(mail);
+            String from = "anhnxps05368@fpt.edu.vn";
+            String subject = "Đặt đơn hàng từ Essence Shop";
+            String body = "Đơn hàng của bạn đã được đặt thành công ! ";
+            helper.setFrom(from, from);
+            helper.setTo(email);
+            helper.setReplyTo(from, from);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            // Gửi mail
+            mailer1.send(mail);
+        } catch (Exception e) {
+            model.addAttribute("message", "Gửi email thất bại !");
+            e.printStackTrace();
+        }
+
         return view(model, request);
     }
 }
